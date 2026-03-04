3798d73ba2f69418a13537d755678b3535226560
Jim Nelin <jim@jine.se>
2026-03-04 17:53:53 +0100

    Tiny fixes, added meta desc and some styling

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 import Header from "@/components/layout/header";
 import Footer from "@/components/layout/footer";
+import "./globals.css";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -16,7 +16,8 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
     title: "Nattsken - Utvecklingsblogg",
-    description: "",
+    description:
+        "Här delar jag mina erfarenheter och insikter från utvecklingsvärlden, inklusive projekt, tekniker och lärdomar.",
 };
 
 export default function RootLayout({
@@ -29,7 +30,7 @@ export default function RootLayout({
             <body
                 className={`${geistSans.variable} ${geistMono.variable} antialiased`}
             >
-                <div className="container max-w-5xl mx-auto py-6">
+                <div className="container max-w-5xl mx-auto p-6">
                     <Header />
                     {children}
                     <Footer />
--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -4,12 +4,18 @@ export default function Header() {
     return (
         <header className="container flex items-center max-w-5xl transparent bg-background/10 backdrop-blur-sm mb-6">
             <div className="flex-col">
-                <h1 className="text-2xl font-bold"><Link href="/">Nattsken</Link></h1>
+                <h1 className="text-2xl font-bold">
+                    <Link href="/">Nattsken</Link>
+                </h1>
                 <h2 className="text-sm text-gray-600">Utvecklingsblogg</h2>
             </div>
 
             <div className="ml-auto max-w-xs w-full">
-                <input type="text" placeholder="Sök efter inlägg..." className="px-4 py-2 w-full border rounded-lg" />
+                <input
+                    type="text"
+                    placeholder="Sök efter inlägg..."
+                    className="px-4 py-2 w-full border rounded-lg"
+                />
             </div>
         </header>
     );

642953f0e0e258c85676c8953f4f1f89694fbd88
Jim Nelin <jim@jine.se>
2026-03-05 00:06:18 +0100

    Midnight coding, added tag-functionality and UI component for it

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import { notFound } from "next/navigation";
 import { getPostBySlug } from "@/lib/posts";
 import { formatDate } from "@/lib/utils";
+import { Tags } from "@/components/ui/tags";
 
 interface PageProps {
     params: {
@@ -20,13 +21,20 @@ export default async function PostPage({ params }: PageProps) {
     // If no post is found, show 404 page
     if (!post) notFound();
 
+    
     return (
-        <article className="max-w-2xl mx-auto px-4 py-8">
+        <article className="max-w-4xl mx-auto py-6 prose prose-lg max-w-none">
+
             <header className="mb-8">
                 <h1 className="text-4xl font-bold mb-2">{post.title}</h1>
-                <time className="text-gray-600">
-                    {formatDate(post.publishDate)}
-                </time>
+                <sub className="text-sm text-gray-500 mb-4 block">{post.summary}</sub>
+
+                <div className="flex items-center gap-4 mb-4">
+                    <time className="text-gray-600" dateTime={post.publishDate?.toISOString()}>
+                        {formatDate(post.publishDate)}
+                    </time>
+                    {post.tags.length > 0 && <>| <div className="text-gray-600">Taggar: <Tags tags={post.tags} /></div></>}
+                </div>
             </header>
 
             <div

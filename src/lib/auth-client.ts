76a72f9223cd4db16f36c568bb5f1e72bdeb9f4a
Jim Nelin <jim@jine.se>
2026-03-10 11:12:38 +0100

    Tons of fixes

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,4 +1,3 @@
-import { createAuthClient } from "better-auth/client";
-// import { createAuthClient } from "better-auth/react" // from docs? make sure to import from better-auth/react
+import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient();
